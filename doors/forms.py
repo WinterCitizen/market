8f19dd46129221537f5496edd2f2a1a30f8c6b93
Daniil Fedyaev <daniil.fedyaev@gs-labs.ru>
2019-04-09 19:29:02 +0300

    Fix compose & add filtering

--- doors/forms.py
+++ doors/forms.py
@@ -13,35 +13,46 @@ class ColorModelForm(forms.ModelForm):
         }
 
 
-class DoorFilterForm(forms.ModelForm):
-    class Meta:
-        model = models.Door
-        exclude = ('price', )
-
-    title = forms.CharField(required=False, label='Название')
-    manufacturer = forms.ModelMultipleChoiceField(
-        queryset=models.Manufacturer.objects.all(),
+class DoorFilterForm(forms.Form):
+    title__icontains = forms.CharField(required=False, label='Название')
+    manufacturer__in = forms.MultipleChoiceField(
         widget=forms.CheckboxSelectMultiple,
         label='Производитель',
+        choices=models.Manufacturer.objects.values_list('id', 'title'),
         required=False)
-    subcategory = forms.ModelMultipleChoiceField(
-        queryset=models.Subcategory.objects.all(),
+    subcategory__in = forms.MultipleChoiceField(
         widget=forms.CheckboxSelectMultiple,
         label='Категория',
+        choices=models.Subcategory.objects.values_list('id', 'title'),
         required=False)
-    frame_material = forms.ModelMultipleChoiceField(
-        queryset=models.FrameMaterial.objects.all(),
+    frame_material__in = forms.MultipleChoiceField(
         widget=forms.CheckboxSelectMultiple,
         label='Каркас полотна',
+        choices=models.FrameMaterial.objects.values_list('id', 'title'),
         required=False)
-    cover_material = forms.ModelMultipleChoiceField(
-        queryset=models.CoverMaterial.objects.all(),
+    cover_material__in = forms.MultipleChoiceField(
         widget=forms.CheckboxSelectMultiple,
         label='Отделка',
+        choices=models.CoverMaterial.objects.values_list('id', 'title'),
         required=False)
-    price_from = forms.IntegerField(
+    heights__in = forms.MultipleChoiceField(
+        widget=forms.CheckboxSelectMultiple,
+        label='Высота',
+        choices=models.Height.objects.values_list('id', 'value'),
+        required=False)
+    widths__in = forms.MultipleChoiceField(
+        widget=forms.CheckboxSelectMultiple,
+        label='Ширина',
+        choices=models.Width.objects.values_list('id', 'value'),
+        required=False)
+    packings__in = forms.MultipleChoiceField(
+        widget=forms.CheckboxSelectMultiple,
+        label='Ширина',
+        choices=models.PackingMaterial.objects.values_list('id', 'title'),
+        required=False)
+    price__gte = forms.IntegerField(
         min_value=0, label='Цена от', required=False)
-    price_to = forms.IntegerField(
+    price__lte = forms.IntegerField(
         min_value=0, label='Цена до', required=False)
     thickness = forms.IntegerField(
         min_value=0, label='Толщина полотна (мм)', required=False)
--- doors/views.py
+++ doors/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.views.generic import ListView, DetailView
+from django.http import HttpResponseBadRequest
 
 from doors.models import Door, Picture
 from doors.forms import DoorFilterForm
@@ -9,14 +10,47 @@ class DoorsListView(ListView):
     template_name = 'doors/list.djt'
     queryset = Door.objects.all().order_by('pk')
     paginate_by = settings.DOORS_PER_PAGE
+    cleaned_data = None
+
+    def get_form(self):
+        if self.request.method == 'POST':
+            return DoorFilterForm(self.request.POST)
+        else:
+            return DoorFilterForm()
+
+    def clean_data(self, cleaned_data):
+        for key, value in cleaned_data.items():
+            if not value:
+                continue
+
+            if isinstance(value, list):
+                value = list(map(int, value))
+
+            yield key, value
+
+    def perform_filter(self, queryset):
+        return queryset.filter(**self.cleaned_data).distinct()
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.cleaned_data is not None:
+            return self.perform_filter(queryset)
+
+        return queryset
 
     def get_context_data(self):
         context = super().get_context_data()
-        context['form'] = DoorFilterForm()
+        context['form'] = self.get_form()
         return context
 
-    def post(self, *args, **kwargs):
-        import ipdb; ipdb.set_trace()
+    def post(self, request, **kwargs):
+        form = self.get_form()
+        if not form.is_valid():
+            return HttpResponseBadRequest()
+        else:
+            self.cleaned_data = dict(self.clean_data(form.cleaned_data))
+
+        return super().get(request, **kwargs)
 
 
 class DoorDetailView(DetailView):
